311d3a6cdcdd7dd86c40f31743b1d9e5fcaf0ee8
rachel <rachelcope.m@gmail.com>
2022-08-26 11:08:22 -0600

    Refractored RouteInfo to Route in layout xml files and Fragment classes.

--- app/src/main/java/com/example/routesuggesterapp/ui/elements/RouteListFragment.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/elements/RouteListFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RouteInfoListFragment.newInstance] factory method to
+ * Use the [RouteListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RouteInfoListFragment : Fragment() {
+class RouteListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class RouteInfoListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_route_info_list, container, false)
+        return inflater.inflate(R.layout.fragment_route_list, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class RouteInfoListFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RouteInfoListFragment().apply {
+            RouteListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
