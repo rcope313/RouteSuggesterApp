5c54259adafc55d3cef69d70eba1835a6f35a0d1
rachel <rachelcope.m@gmail.com>
2022-08-30 10:31:58 -0600

    Added class fields of ResponsiveRoute. I decided to assign the favorite fields as a var for mutability. I see in the future that might be changing.

--- app/src/main/java/com/example/routesuggesterapp/data/repo/ResponsiveRoute.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/repo/ResponsiveRoute.kt
@@ -2,5 +2,42 @@ package com.example.routesuggesterapp.data.repo
 
 import com.example.routesuggesterapp.data.network.Route
 
-class ResponsiveRoute() : Route() {
-}
+class ResponsiveRoute(
+    var favorite: Boolean,
+    id: Int,
+    routeName: String,
+    mountainName: String,
+    isStandardRoute: Boolean, 
+    isSnowRoute: Boolean,
+    grade: Int,
+    trailhead: String,
+    summitElevation: Int,
+    gain: Int,
+    length: Int,
+    exposure: String,
+    rockfallPotential: String,
+    routeFinding: String,
+    commitment: String,
+    roadDifficulty: Int,
+    routeUrl: String,
+    trailheadUrl: String
+)
+    : Route(
+    id,
+    routeName,
+    mountainName,
+    isStandardRoute,
+    isSnowRoute,
+    grade,
+    trailhead,
+    summitElevation,
+    gain,
+    length,
+    exposure,
+    rockfallPotential,
+    routeFinding,
+    commitment,
+    roadDifficulty,
+    routeUrl,
+    trailheadUrl,
+)
