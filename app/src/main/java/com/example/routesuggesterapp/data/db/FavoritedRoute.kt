eef0b75a4ac7761e23873fd82c02a2a3b2621d51
rachel <rachelcope.m@gmail.com>
2022-08-30 10:37:50 -0600

    Added class fiels to Entity class FavoritedRoute.

--- app/src/main/java/com/example/routesuggesterapp/data/db/FavoritedRoute.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/db/FavoritedRoute.kt
@@ -6,5 +6,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class FavoritedRoute (
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0
+    val id: Int,
+    val routeId: Int,
+    val isFavorited: Boolean = true
 )
