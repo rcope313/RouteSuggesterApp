58a18c72c45dcc83775ddd7b78ad93aff828612c
rachel <rachelcope.m@gmail.com>
2022-08-30 10:09:25 -0600

    Added class fields to Rotue

--- app/src/main/java/com/example/routesuggesterapp/data/network/Route.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/network/Route.kt
@@ -1,5 +1,25 @@
 package com.example.routesuggesterapp.data.network
 
-open class Route {
+open class Route(
+    val id: Int,
+    val routeName: String,
+    val mountainName: String,
+    val isStandardRoute: Boolean,
+    val isSnowRoute: Boolean,
+    val grade: Int,
+    val trailhead: String,
+    val summitElevation: Int,
+    val gain: Int,
+    val length: Int,
+    val exposure: String,
+    val rockfallPotential: String,
+    val routeFinding: String,
+    val commitment: String,
+    val roadDifficulty: Int,
+    val routeUrl: String,
+    val trailheadUrl: String
+)
 
-}
+
+
+
