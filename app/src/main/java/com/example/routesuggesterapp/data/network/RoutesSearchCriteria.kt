70ae440adc661b81a016f5ca619ac508751bb10e
rachel <rachelcope.m@gmail.com>
2022-09-14 09:05:26 -0600

    I created a builder pattern for RoutesSearchCritiera - I was hoping to use the Immutables Library, but conversion to Kotlin proved to not be possible. I thought the builder pattern would make it obvious to the reader that all the initialization of the class fields are optional, and once built, is immutable. I stayed away from using an Optional<> container, with Kotlin's ability of null-saftey.

--- app/src/main/java/com/example/routesuggesterapp/data/network/RoutesSearchCriteria.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/network/RoutesSearchCriteria.kt
@@ -1,20 +1,66 @@
 package com.example.routesuggesterapp.data.network
 
-import java.util.*
+class RoutesSearchCriteria private constructor (
+    val routeName: String?,
+    val mountainName: String?,
+    val sStandardRoute: Boolean?,
+    val isSnowRoute: Boolean?,
+    val grade: Int?,
+    val trailhead: String?,
+    val summitElevation: Int?,
+    val gain: Int?,
+    val length: Int?,
+    val exposure: String?,
+    val rockfallPotential: String?,
+    val routeFinding: String?,
+    val commitment: String?,
+    val roadDifficulty: Int?) {
 
-data class RoutesSearchCriteria(
-    val routeName: Optional<String>,
-    val mountainName: Optional<String>,
-    val isStandardRoute: Optional<Boolean>,
-    val isSnowRoute: Optional<Boolean>,
-    val grade: Optional<Int>,
-    val trailhead: Optional<String>,
-    val summitElevation: Optional<Int>,
-    val gain: Optional<Int>,
-    val length: Optional<Int>,
-    val exposure: Optional<String>,
-    val rockfallPotential: Optional<String>,
-    val routeFinding: Optional<String>,
-    val commitment: Optional<String>,
-    val roadDifficulty: Optional<String>
-)
+    data class Builder(
+        var routeName: String? = null,
+        var mountainName: String? = null,
+        var isStandardRoute: Boolean? = null,
+        var isSnowRoute: Boolean? = null,
+        var grade: Int? = null,
+        var trailhead: String? = null,
+        var summitElevation: Int? = null,
+        var gain: Int? = null,
+        var length: Int? = null,
+        var exposure: String? = null,
+        var rockfallPotential: String? = null,
+        var routeFinding: String? = null,
+        var commitment: String? = null,
+        var roadDifficulty: Int? = null
+    ) {
+
+        fun routeName(routeName: String) = apply { this.routeName = routeName }
+
+        fun mountainName(mountainName: String) = apply { this.mountainName = mountainName }
+
+        fun isStandardRoute(isStandardRoute: Boolean) = apply { this.isStandardRoute = isStandardRoute }
+
+        fun isSnowRoute(isSnowRoute: Boolean) = apply { this.isSnowRoute = isSnowRoute }
+
+        fun grade(grade: Int) = apply { this.grade = grade }
+
+        fun trailhead(trailhead: String) = apply { this.trailhead = trailhead }
+
+        fun summitElevation(summitElevation: Int) = apply { this.summitElevation = summitElevation }
+
+        fun gain(gain: Int) = apply { this.gain = gain }
+
+        fun length(length: Int) = apply { this.length = length }
+
+        fun exposure(exposure: String) = apply { this.exposure = exposure }
+
+        fun rockfallPotential(rockfallPotential: String) = apply { this.rockfallPotential = rockfallPotential }
+
+        fun routeFinding(routeFinding: String) = apply { this.routeFinding = routeFinding}
+
+        fun commitment(commitment: String) = apply { this.commitment = commitment }
+
+        fun roadDifficulty(roadDifficulty: Int) = apply { this.roadDifficulty = roadDifficulty}
+
+        fun build() = RoutesSearchCriteria(routeName, mountainName, isStandardRoute, isSnowRoute, grade, trailhead, summitElevation, gain, length, exposure, rockfallPotential, routeFinding, commitment, roadDifficulty)
+    }
+}
