1a7c3e3d3e90112184754fab071bec6d717e1f49
rachel <rachelcope.m@gmail.com>
2022-09-15 09:26:35 -0600

    Refractored parent directory of MainAcitivty, and added @AndroidEntryPoint annotation to MainActivity and all fragments

--- app/src/main/java/com/example/routesuggesterapp/ui/elements/RouteFragment.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/elements/RouteFragment.kt
@@ -6,17 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.routesuggesterapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RouteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class RouteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
