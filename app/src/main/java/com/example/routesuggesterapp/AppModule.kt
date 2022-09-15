26577d05739aea23554618cdb09fcaf453c39254
rachel <rachelcope.m@gmail.com>
2022-09-15 09:39:23 -0600

    Added empty AppModule object

---
+++ app/src/main/java/com/example/routesuggesterapp/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.routesuggesterapp
+
+import android.app.Application
+import dagger.Module
+import dagger.hilt.InstallIn
+
+@Module
+@InstallIn(Application::class)
+object AppModule {
+}
