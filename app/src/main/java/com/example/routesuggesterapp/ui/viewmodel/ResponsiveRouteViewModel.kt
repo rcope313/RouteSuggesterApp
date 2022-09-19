9c81ce6abfe713a70d31d9b2766b65d240119272
rachel <rachelcope.m@gmail.com>
2022-09-19 11:56:45 -0600

    Added class fields _routes and routes

--- app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.routesuggesterapp.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.routesuggesterapp.data.repo.ResponsiveRoute
 import com.example.routesuggesterapp.data.repo.ResponsiveRouteRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,6 +12,9 @@ import javax.inject.Inject
 class ResponsiveRouteViewModel @Inject constructor(
     private val repo: ResponsiveRouteRepo
 ) : ViewModel() {
+    private val _routes = MutableLiveData<List<ResponsiveRoute>>()
+    val routes: LiveData<List<ResponsiveRoute>> = _routes
+
 
 
 }
