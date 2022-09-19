585afd99c23947e9f66f9421d5fce0a6c990500c
rachel <rachelcope.m@gmail.com>
2022-09-19 11:55:06 -0600

    Refractored ViewModel with @HiltViewModel annotation and @Inject constructor with private val ResponsiveRouteRepo

--- app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.routesuggesterapp.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.routesuggesterapp.data.repo.ResponsiveRoute
+import com.example.routesuggesterapp.data.repo.ResponsiveRouteRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ResponsiveRouteViewModel : ViewModel() {
-    private val _routes = MutableLiveData<List<ResponsiveRoute>>()
-    val routes: LiveData<List<ResponsiveRoute>> = _routes
+@HiltViewModel
+class ResponsiveRouteViewModel @Inject constructor(
+    private val repo: ResponsiveRouteRepo
+) : ViewModel() {
+
 
 }
