2d0d3f88e138bb0cfebc4d1a2c96e10a58854bcf
rachel <rachelcope.m@gmail.com>
2022-09-19 10:11:55 -0600

    Refractored RouteApiService to remove const val builders and RouteApi object due to project refraction with AppModule di.

--- app/src/main/java/com/example/routesuggesterapp/data/network/RouteApiService.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/network/RouteApiService.kt
@@ -1,19 +1,6 @@
 package com.example.routesuggesterapp.data.network
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.POST
-
-private const val BASE_URL = "a-url"
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL)
-    .build()
 
 interface RouteApiService {
 
@@ -23,9 +10,4 @@ interface RouteApiService {
     @POST("search/suggest_by_weather")
     suspend fun getRoutesBySearchCriteriaAndWeather(criteria: RoutesSearchCriteria) : List<Route>
 
-}
-
-object RouteApi {
-    val service : RouteApiService by lazy {
-        retrofit.create(RouteApiService::class.java) }
 }
