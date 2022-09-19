44507a60b8fe4d60845e46f9d806a1ab3aeb84a1
rachel <rachelcope.m@gmail.com>
2022-09-19 10:13:37 -0600

    Refractored FavoritedRouteDatabase and removed companion object due to project refractore with AppModule di

--- app/src/main/java/com/example/routesuggesterapp/data/db/FavoritedRouteDatabase.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/db/FavoritedRouteDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.routesuggesterapp.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [FavoritedRoute::class], version = 1, exportSchema = false)
@@ -10,22 +8,4 @@ abstract class FavoritedRouteDatabase : RoomDatabase() {
 
     abstract fun favoritedRouteDao(): FavoritedRouteDao
 
-    companion object {
-        @Volatile
-        private var INSTANCE: FavoritedRouteDatabase? = null
-
-        fun getDatabase(context: Context): FavoritedRouteDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    FavoritedRouteDatabase::class.java,
-                    "favorited_route_database"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
 }
