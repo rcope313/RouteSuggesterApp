55ee366eaa7b011260639606b3c08ec9f2537a3e
rachel <rachelcope.m@gmail.com>
2022-09-20 11:28:51 -0600

    Added RecyclerView to fragment_filter.xml

--- app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
@@ -3,9 +3,12 @@ package com.example.routesuggesterapp.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.routesuggesterapp.data.network.RoutesSearchCriteria
 import com.example.routesuggesterapp.data.repo.ResponsiveRoute
 import com.example.routesuggesterapp.data.repo.ResponsiveRouteRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,6 +18,16 @@ class ResponsiveRouteViewModel @Inject constructor(
     private val _routes = MutableLiveData<List<ResponsiveRoute>>()
     val routes: LiveData<List<ResponsiveRoute>> = _routes
 
+    fun getRoutesByCriteriaAndWeather(criteria: RoutesSearchCriteria) {
+        viewModelScope.launch {
+            _routes.value = repo.getResponsiveRoutesBySearchCriteriaAndWeather(criteria)
+        }
+    }
 
+    fun getRoutesByCriteria(criteria: RoutesSearchCriteria) {
+        viewModelScope.launch {
+            _routes.value = repo.getResponsiveRoutesBySearchCriteria(criteria)
+        }
+    }
 
 }
