4530009d6d9f00e2a353ebeac30bf70d82995d9a
rachel <rachelcope.m@gmail.com>
2022-09-21 09:36:14 -0600

    Added boilerplate of FilterFragmentAdapter with AsyncListDiffer to compute difference between two lists for faster computation

---
+++ app/src/main/java/com/example/routesuggesterapp/ui/adapter/FilterFragmentAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.routesuggesterapp.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.routesuggesterapp.data.repo.ResponsiveRoute
+
+class FilterFragmentAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val  mDiffer: AsyncListDiffer<ResponsiveRoute> = AsyncListDiffer(this, DiffCallback);
+
+    override fun getItemCount() =
+        mDiffer.currentList.size
+
+    fun submitList(list: List<ResponsiveRoute>) {
+        mDiffer.submitList(list)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val user = mDiffer.currentList[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<ResponsiveRoute>() {
+            override fun areItemsTheSame(oldItem: ResponsiveRoute, newItem: ResponsiveRoute): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: ResponsiveRoute, newItem: ResponsiveRoute): Boolean {
+                TODO()
+            }
+        }
+    }
+
+}
