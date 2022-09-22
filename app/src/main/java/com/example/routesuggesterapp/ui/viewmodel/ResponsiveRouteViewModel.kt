beb2531b1af5c6e9513b2e8718477b4b9cd563e3
rachel <rachelcope.m@gmail.com>
2022-09-22 13:09:15 -0600

    Added search criteria as class fields in ResponsiveRouteViewModel

--- app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/viewmodel/ResponsiveRouteViewModel.kt
@@ -3,6 +3,7 @@ package com.example.routesuggesterapp.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.routesuggesterapp.data.network.RoutesSearchCriteria
 import com.example.routesuggesterapp.data.repo.ResponsiveRoute
 import com.example.routesuggesterapp.data.repo.ResponsiveRouteRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,5 +15,11 @@ class ResponsiveRouteViewModel @Inject constructor(
 ) : ViewModel() {
     private val _routes = MutableLiveData<List<ResponsiveRoute>>()
     val routes: LiveData<List<ResponsiveRoute>> = _routes
+
+    private val _searchCriteria = MutableLiveData<RoutesSearchCriteria>()
+    val searchCriteria: LiveData<RoutesSearchCriteria> = _searchCriteria
+
+    fun updateSearchCriteria(builder: RoutesSearchCriteria.Builder) =
+        builder.build()
 
 }
