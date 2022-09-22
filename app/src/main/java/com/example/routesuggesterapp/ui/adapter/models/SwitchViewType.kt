648f1814be3cbcc83dbcbb4288e143b022752371
rachel <rachelcope.m@gmail.com>
2022-09-22 11:41:46 -0600

    I added a constructor parameter to SwitchViewType isChecked:Boolean. Used databinding in xml file for android:checked=@{switchViewType.checked}

--- app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/SwitchViewType.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/SwitchViewType.kt
@@ -1,5 +1,8 @@
 package com.example.routesuggesterapp.ui.adapter.models
 
-class SwitchViewType(override val title: String) : FilterViewType() {
+class SwitchViewType(
+    override val title: String,
+    val isChecked: Boolean,
+    ) : FilterViewType() {
     override val viewType = ViewType.SWITCH
 }
