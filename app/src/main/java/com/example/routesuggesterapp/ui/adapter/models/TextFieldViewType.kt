2ef670a823e40cdfe4106f263891ad8b73979236
rachel <rachelcope.m@gmail.com>
2022-09-22 12:22:19 -0600

    Included editTextFieldHint constructor paramter to TextFieldViewType for databinding with TextInputEditText android:hint

--- app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/TextFieldViewType.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/TextFieldViewType.kt
@@ -1,5 +1,8 @@
 package com.example.routesuggesterapp.ui.adapter.models
 
-class TextFieldViewType(override val title: String) : FilterViewType() {
+class TextFieldViewType(
+    override val title: String,
+    val editTextFieldHint: String
+    ) : FilterViewType() {
     override val viewType = ViewType.TEXTFIELD
 }
