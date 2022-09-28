46850e192c329ba05daa486b6ba876a16f69e4d6
rachel <rachelcope.m@gmail.com>
2022-09-28 17:10:45 -0600

    Refractored constructor type of all implementing clases of FilterViewType to Criteria from String

--- app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/SwitchViewType.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/SwitchViewType.kt
@@ -1,7 +1,9 @@
 package com.example.routesuggesterapp.ui.adapter.models
 
+import com.example.routesuggesterapp.data.network.Criteria
+
 class SwitchViewType(
-    override val title: String,
+    override val title: Criteria,
     val isChecked: Boolean,
     ) : FilterViewType() {
     override val viewType = ViewType.SWITCH
