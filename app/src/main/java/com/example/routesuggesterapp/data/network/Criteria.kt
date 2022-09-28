8af15d73047bb69ad1348f39838fb09f561e3fba
rachel <rachelcope.m@gmail.com>
2022-09-28 17:04:41 -0600

    Updated title class field of FitlerViewType to be of Criteria type. Overrode toString of Criteria enum for databinding

--- app/src/main/java/com/example/routesuggesterapp/data/network/Criteria.kt
+++ app/src/main/java/com/example/routesuggesterapp/data/network/Criteria.kt
@@ -4,6 +4,25 @@ enum class Criteria {
     ROUTE_NAME, MOUNTAIN_NAME, IS_STANDARD_ROUTE, IS_SNOW_ROUTE, GRADE, TRAILHEAD, SUMMIT_ELEVATION,
     GAIN, LENGTH, EXPOSURE, ROCKFALL_POTENTIAL, ROUTE_FINDING, COMMITMENT, ROAD_DIFFICULTY;
 
+    override fun toString(): String {
+        return when (this) {
+            ROUTE_NAME -> "Route Name"
+            MOUNTAIN_NAME -> "Mountain Name"
+            IS_STANDARD_ROUTE -> "Standard Route Only"
+            IS_SNOW_ROUTE ->  "Snow Route Only"
+            GRADE -> "Grade"
+            TRAILHEAD -> "Trailhead"
+            SUMMIT_ELEVATION -> "Summit Elevation"
+            GAIN -> "Gain"
+            LENGTH -> "Length"
+            EXPOSURE -> "Exposure"
+            ROCKFALL_POTENTIAL -> "Rockfall Potential"
+            ROUTE_FINDING -> "Route Finding"
+            COMMITMENT -> "Commitment"
+            ROAD_DIFFICULTY -> "Road Difficulty"
+        }
+    }
+
     fun fromString(criteria: String): Criteria {
        return when (criteria) {
            "Route Name" -> ROUTE_NAME
--- app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/FilterViewType.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/FilterViewType.kt
@@ -1,6 +1,8 @@
 package com.example.routesuggesterapp.ui.adapter.models
 
+import com.example.routesuggesterapp.data.network.Criteria
+
 abstract class FilterViewType {
     abstract val viewType: ViewType
-    abstract val title: String
+    abstract val title: Criteria
 }
