2d48c3ba4255aca9c3088a1328b0137d93ab0d53
rachel <rachelcope.m@gmail.com>
2022-09-29 09:22:49 -0600

    Added static list of FilterViewTypeList for FilterFragmentAdapter injection

--- app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/FilterViewTypeList.kt
+++ app/src/main/java/com/example/routesuggesterapp/ui/adapter/models/FilterViewTypeList.kt
@@ -1,5 +1,22 @@
 package com.example.routesuggesterapp.ui.adapter.models
 
+import com.example.routesuggesterapp.data.network.Criteria
+
 object FilterViewTypeList {
-    val list = arrayListOf<FilterViewType>()
+    val list = arrayListOf(
+        TextFieldViewType(Criteria.MOUNTAIN_NAME, "Long's Peak"),
+        TextFieldViewType(Criteria.ROUTE_NAME, "Keyhole Route"),
+        SwitchViewType(Criteria.IS_STANDARD_ROUTE, true),
+        SwitchViewType(Criteria.IS_SNOW_ROUTE, false),
+        SliderViewType(Criteria.GRADE, 0, 5, listOf(0,1)),
+        TextFieldViewType(Criteria.TRAILHEAD, "Long's Peak Trailhead"),
+        SliderViewType(Criteria.SUMMIT_ELEVATION, 14000, 14438, listOf(14000, 14438)),
+        SliderViewType(Criteria.GAIN, 0, 10000, listOf(0, 1000)),
+        SliderViewType(Criteria.LENGTH, 0, 50, listOf(0, 5)),
+        ChipViewType(Criteria.EXPOSURE),
+        ChipViewType(Criteria.ROCKFALL_POTENTIAL),
+        ChipViewType(Criteria.ROUTE_FINDING),
+        ChipViewType(Criteria.COMMITMENT),
+        SliderViewType(Criteria.ROAD_DIFFICULTY, 0, 6, listOf(0,1))
+    )
 }
